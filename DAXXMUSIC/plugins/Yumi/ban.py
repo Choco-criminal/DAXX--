c38d61d215eb02d942750b006d39efebd26ec61c
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-10-31 17:33:06 +0530

    Update ban.py

--- DAXXMUSIC/plugins/Yumi/ban.py
+++ DAXXMUSIC/plugins/Yumi/ban.py
@@ -14,13 +14,13 @@ from pyrogram.types import (
 from string import ascii_lowercase
 from typing import Dict, Union
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.core.mongo import mongodb
+from DAXXMUSIC import app
+from DAXXMUSIC.misc import SUDOERS
+from DAXXMUSIC.core.mongo import mongodb
 from utils.error import capture_err
-from VIPMUSIC.utils.keyboard import ikb
-from VIPMUSIC.utils.database import save_filter
-from VIPMUSIC.utils.functions import (
+from DAXXMUSIC.utils.keyboard import ikb
+from DAXXMUSIC.utils.database import save_filter
+from DAXXMUSIC.utils.functions import (
     extract_user,
     extract_user_and_reason,
     time_converter,
