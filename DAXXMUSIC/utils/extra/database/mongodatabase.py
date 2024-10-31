ebb89cd20342eb7298697158cede52f52ab8c300
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-10-31 19:19:38 +0530

    Update mongodatabase.py

--- DAXXMUSIC/utils/extra/database/mongodatabase.py
+++ DAXXMUSIC/utils/extra/database/mongodatabase.py
@@ -10,7 +10,7 @@
 
 from typing import Dict, List, Union
 
-from VIPMUSIC.core.mongo import mongodb
+from DAXXMUSIC.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
